ee2b1e1925a28d22ab2933eebee60bde188688a0
Ryan Vicente <147751342+devRyanVicente@users.noreply.github.com>
2024-02-01 16:30:57 -0300

    Arquivos Principais para o Programa
    Feito no Visual Studio 2022

---
+++ Aplicativo base/Login.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Aplicativo_base
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.cmdSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.CMD_Senha = new System.Windows.Forms.TextBox();
+            this.cmdLogin = new System.Windows.Forms.TextBox();
+            this.pnlLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.SystemColors.Info;
+            this.pnlLogin.BackgroundImage = global::Aplicativo_base.Properties.Resources.Login;
+            this.pnlLogin.Controls.Add(this.cmdSenha);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.CMD_Senha);
+            this.pnlLogin.Controls.Add(this.cmdLogin);
+            this.pnlLogin.Location = new System.Drawing.Point(431, 245);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(317, 221);
+            this.pnlLogin.TabIndex = 1;
+            this.pnlLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlLogin_Paint);
+            // 
+            // cmdSenha
+            // 
+            this.cmdSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cmdSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSenha.Location = new System.Drawing.Point(68, 98);
+            this.cmdSenha.Name = "cmdSenha";
+            this.cmdSenha.Size = new System.Drawing.Size(228, 19);
+            this.cmdSenha.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(37, 165);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(238, 53);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Entrar";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CMD_Senha
+            // 
+            this.CMD_Senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.CMD_Senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CMD_Senha.Location = new System.Drawing.Point(68, 98);
+            this.CMD_Senha.Name = "CMD_Senha";
+            this.CMD_Senha.PasswordChar = '*';
+            this.CMD_Senha.Size = new System.Drawing.Size(228, 19);
+            this.CMD_Senha.TabIndex = 3;
+            this.CMD_Senha.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmdLogin
+            // 
+            this.cmdLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cmdLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdLogin.Location = new System.Drawing.Point(68, 31);
+            this.cmdLogin.Name = "cmdLogin";
+            this.cmdLogin.Size = new System.Drawing.Size(228, 19);
+            this.cmdLogin.TabIndex = 2;
+            this.cmdLogin.WordWrap = false;
+            this.cmdLogin.TextChanged += new System.EventHandler(this.cmdLogin_TextChanged);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pnlLogin);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "frmLogin";
+            this.Text = " ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmLogin_KeyDown);
+            this.Resize += new System.EventHandler(this.frmLogin_Resize);
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.TextBox cmdLogin;
+        private System.Windows.Forms.TextBox CMD_Senha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox cmdSenha;
+    }
+}
---
+++ Aplicativo base/Login.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Aplicativo_base
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            pnlLogin.Visible = false;
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            pnlLogin.Location = new Point(this.Width / 2 - 158, this.Height / 2 - 124);
+            pnlLogin.Visible = true;
+            btnLogin.FlatAppearance.MouseOverBackColor = Color.FromArgb(173, 216, 255);
+            btnLogin.FlatAppearance.MouseDownBackColor = Color.FromArgb(124, 216, 230);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChamarLogin();
+            //codigo login! vef no MYSQL
+
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void PnlLogin_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void cmdLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && cmdLogin.Text != "")
+            {
+                cmdSenha.Focus();
+                Thread.Sleep(350);
+                
+            }
+            if (e.KeyCode == Keys.Enter && cmdSenha.Text != "")
+            {
+                ChamarLogin();
+               
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                cmdSenha.Focus();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                cmdLogin.Focus();
+            }
+        }
+        private void ChamarLogin()
+        {
+            if (cmdLogin.Text == "" || cmdSenha.Text == "")
+            {
+                notifyIcon1.ShowBalloonTip(2000, "ERRO", "Não deixe os Campos Vazios!", ToolTipIcon.Info);
+                return;
+            }
+
+            if (cmdLogin.Text == "teste" && cmdSenha.Text == "123")
+            {
+                Limpar();
+                notifyIcon1.ShowBalloonTip(2000, "Sucesso", "Logado com Sucesso!", ToolTipIcon.Info);
+                
+            }
+            else
+            {
+                notifyIcon1.ShowBalloonTip(2000, "ERRO", "Nome/Senha Invalida!", ToolTipIcon.Info);
+                return;
+            }
+        }
+        
+        private void Limpar()
+        {
+            cmdLogin.Text = "";
+            cmdSenha.Text = "";
+            cmdLogin.Focus();
+        }
+
+        private void frmLogin_Resize(object sender, EventArgs e)
+        {
+            pnlLogin.Location = new Point(this.Width / 2 - 158, this.Height / 2 - 124);
+        }
+    }
+}
+
